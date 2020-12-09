d8d42bf91449dffaca1996a69ed8f317ae22dc8d
Rayleigh761 <62617420+Rayleigh761@users.noreply.github.com>
2020-12-09 18:00:32 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,39 @@
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme") //Responsavel em fazer a mudança de cores
+
+
+const getStyle = (element,style) =>
+    window
+        .getComputedStyle(element)
+        .getPropertyValue(style)
+
+
+const initialColors = {
+    bg: getStyle(html, "--bg"),
+    bgPanel: getStyle(html, "--bg-panel"),
+    colorHeadings: getStyle(html, "--color-headings"),
+    colorText: getStyle(html, "--color-text"),
+}
+
+
+const darkmode = {
+    bg: "#333333",
+    bgPanel: "#434343",
+    colorHeadings: "#3664FF",
+    colorText: "#B5B5B5"
+}
+
+    const tranformeKey = key => 
+        "--" + key.replace(/([A-Z])/,"-$1").toLowerCase()
+
+
+const changeColors = (colors) => {
+    Object.keys(colors).map(key =>
+       html.style.setProperty(tranformeKey(key) , colors[key]) 
+       ) 
+}
+
+checkbox.addEventListener("change", ({target}) => {
+    target.checked ? changeColors(darkmode) : changeColors(initialColors)
+})
+
